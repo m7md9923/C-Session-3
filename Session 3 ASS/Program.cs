54c265587f46f1dc60c53df58a087ca6c8c2661d
m7md9923 <mhmdnsrbrkat0@gmail.com>
2025-06-26 17:27:44 +0300

    Q1

---
+++ Session 3 ASS/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Session_3_ASS;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Question 1
+        /*
+            Write a program that allows the user to enter a number then print it.
+        */
+        Console.WriteLine("Plz Enter a number: ");
+        int num;
+        bool isNum = int.TryParse(Console.ReadLine(), out num);
+        if(isNum) 
+            Console.WriteLine($"The Num is : {num}");
+        else 
+            Console.WriteLine("Invalid Input");
+        #endregion
+    }
+}
