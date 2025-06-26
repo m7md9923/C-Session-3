24b299b83000c4e080a341f70f735ac1b5873de7
m7md9923 <mhmdnsrbrkat0@gmail.com>
2025-06-27 00:02:20 +0300

    Task

---
+++ Session 3 ASS/Task.cs
@@ -0,0 +1,49 @@
+﻿namespace Session_3_ASS;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Task
+        
+        /*
+            Task:
+           Ask user for salary amount and print:
+           "No tax" if < 5000 , and the salary stays the same.
+           "10% tax" if between 5000–9999 ,  subtract 10% tax.
+           "20% tax" if ≥ 10000 ,  subtract 20% tax.
+           Expected output :
+           ==> 10% tax applied
+           ==> Final salary after tax = 7200
+         */
+        //     double salary;
+        //     Console.WriteLine("plz Enter the Salary : ");
+        //     while (!Double.TryParse(Console.ReadLine(), out salary) || salary < 0)
+        //     {
+        //         Console.WriteLine("invalid salary. Try again");
+        //     }
+        //     
+        //     if (salary < 5000)
+        //     {
+        //         Console.WriteLine("No tax");
+        //         Console.WriteLine("Final salary after tax = " + salary);
+        //     }
+        //     else if (salary >= 5000 && salary <= 9999)
+        //     {
+        //         double tax = salary * 0.1;
+        //         double TotalSalary = salary - tax;
+        //         Console.WriteLine("==> 10% tax applied");
+        //         Console.WriteLine("==> Final salary after tax = " + TotalSalary);
+        //     }
+        //     else 
+        //     {
+        //         double tax = salary * 0.2;
+        //         double TotalSalary = salary - tax;
+        //         Console.WriteLine("==> 20% tax applied");
+        //         Console.WriteLine("==> Final salary after tax = " + TotalSalary);
+        //     }
+        // }
+    
+        #endregion
+    }
+}
